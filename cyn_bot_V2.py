5356893d6bcf329676cda04ea912fdd68d0bc3be
ImDaGoatCreates <hudsonhuckeba0@gmail.com>
2025-03-23 17:47:49 -0400

    Update cyn_bot_V2.py

--- cyn_bot_V2.py
+++ cyn_bot_V2.py
@@ -1,6 +1,7 @@
 #THIS IS IMPORTANT, AND THE CODE WILL ONLY WORK IF YOU DO THIS PROPERLY. The way to get the code to run is to type the following in the terminal and hit ENTER:
 #python3 cyn_bot_V2.py
 #Type that in the terminal EXACTLY otherwise it won't work and hit ENTER to run the code
+#The program will be full-screen, so to end the program, click on the PyGame window and hit ALT + F4
 
 import pygame
 import tkinter as tk
